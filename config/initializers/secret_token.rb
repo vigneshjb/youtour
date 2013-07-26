9b5568859aed7ee8cf0292105e0c206f264ccbe9
Vignesh Jayabalan <vigneshj@freshdesk.com>
2013-07-26 07:02:37 +0530

    without models and controllers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youtour::Application.config.secret_token = 'c57caf167bf8a89761bd8c806d2c399a859a45c79ce8082092e40ec5bd84c9d312974114ae8470fba47ea0a2b08f86d3f0bd1eb8b7fa87ce706b7af87453f0fa'
